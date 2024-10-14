edf5a87b4cc1ed37467528397e53de4285fd81fb
faty niang <fatyniang128@gmail.com>
2024-10-14 10:21:12 +0100

    refact: reset entire project

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing 
+        elit. Dolorum hic quam obcaecati quibusdam consequatur 
+        a sed mollitia necessitatibus reprehenderit et.
+      </p>
+    </div>
+  )
+}
+
+export default HomePage;
